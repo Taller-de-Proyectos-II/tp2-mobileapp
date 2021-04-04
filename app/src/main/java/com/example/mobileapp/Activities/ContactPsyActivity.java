2f45ff9e5d39af9c9cf986c7c735f7f82539b86b
Deiroo <renatoballon.rb@gmail.com>
2021-04-04 18:08:07 -0500

    INTERFACES HU08 HU09 HU13 completadas

--- app/src/main/java/com/example/mobileapp/Activities/ContactPsyActivity.java
+++ app/src/main/java/com/example/mobileapp/Activities/ContactPsyActivity.java
@@ -1,16 +1,38 @@
 package com.example.mobileapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.mobileapp.R;
 
 public class ContactPsyActivity extends AppCompatActivity {
 
+    ImageView ivToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_psy);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                showPopup(v);
+            }
+        });
+        getSupportActionBar().setTitle("Contacta a tu Psicólogo");
+
+        ivToolbar = findViewById(R.id.ivPerfil);
+        ivToolbar.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void showPopup(View v) {
     }
 }
